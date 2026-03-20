82e37db29ff1fa74fb3a1c9b25ac982a57df7c04
Sander Tamm <162053604+TheBighi@users.noreply.github.com>
2026-03-20 09:51:55 +0000

    kuva saadud andmed MealItem komponendi abil

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,15 +1,19 @@
+import './MealItem.css'
+
 const MealItem = (props) => {
+    console.log('PROPS ARE HERE ')
+    
     return (
-        <li>
-            <article>
-                <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
+        <li className='meal-item'>
+            <article >
+                <img src={require(`../assets/${props.mealData.image}`)} alt={props.mealData.name}/>
                 <div>
-                    <h3>{props.meal.name}</h3>
-                    <p>props.meal.price</p>
-                    <p>{props.meal.description}</p>
+                    <h3>{props.mealData.name}</h3>
+                    <p className='meal-item-price'>{props.mealData.price}</p>
+                    <p className='meal-item-description'>{props.mealData.description}</p>
                 </div>
                 <p>
-                    <button >Add to Cart</button>
+                    <button className='meal-item-actions'>Add to Cart</button>
                 </p>
             </article>
         </li>
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,21 +1,26 @@
 import { useEffect, useState } from "react"
+import MealItem from './MealItem'
 
 const Meals = () => {
-
     const [meals, setMeals] = useState([])
+
+    let mealContent = <p>No meals found</p>
 
     useEffect(() => {
         fetch('https://curly-guacamole-x5x69vgg9gxhvvgw-3001.app.github.dev/meals').then(res => res.json()).then(data => setMeals(data))
     }, [])
 
+    mealContent = meals.map((meal) => (
+        <MealItem mealData={meal}/>
+    ))
 
-    console.log(meals)
+
     return (
         <>
         <h2>create list of meals, using fetch data from backend</h2>
         <ul id="meals">
             { 
-                // list of meals
+                mealContent
             }
         </ul>
         </>
