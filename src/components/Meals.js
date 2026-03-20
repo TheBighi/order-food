20da2efb3bc3026b7457039adbc8490fb83ceb51
Sander Tamm <162053604+TheBighi@users.noreply.github.com>
2026-03-20 08:40:06 +0000

    Header and Meals kompoentide lisamine rakendusele

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,10 +1,13 @@
 const Meals = () => {
     return (
+        <>
+        <h2>create list of meals, using fetch data from backend</h2>
         <ul id="meals">
             { 
                 // list of meals
             }
         </ul>
+        </>
     )
 }
 
